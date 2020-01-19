479454f95cc95773e8f2b8eb5992f8c234b89633
Vikas Pogu <vikaspoguadf@gmail.com>
2020-01-19 13:49:26 -0500

    fourth commit

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/hybridgroup/mjpeg"
 	"gocv.io/x/gocv"
+	"image"
+	"image/color"
 	"log"
 	"net/http"
 )
@@ -41,6 +43,13 @@ func capture() {
 	img := gocv.NewMat()
 	defer img.Close()
 
+	xmlFile := "./data/haarcascade_frontalface_default.xml"
+	// load classifier to recognize faces
+	classifier := gocv.NewCascadeClassifier()
+	defer classifier.Close()
+
+	classifier.Load(xmlFile)
+
 	for {
 		if ok := webcam.Read(&img); !ok {
 			fmt.Printf("cannot read device %d\n", deviceID)
@@ -48,6 +57,22 @@ func capture() {
 		}
 		if img.Empty() {
 			continue
+		}
+
+		// detect faces
+		rects := classifier.DetectMultiScale(img)
+
+		// color for the rect when faces detected
+		blue := color.RGBA{0, 0, 255, 0}
+
+		// draw a rectangle around each face on the original image,
+		// along with text identifing as "Human"
+		for _, r := range rects {
+			gocv.Rectangle(&img, r, blue, 3)
+
+			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 2, 2)
+			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 2, blue, 2)
 		}
 
 		buf, _ := gocv.IMEncode(".jpg", img)
