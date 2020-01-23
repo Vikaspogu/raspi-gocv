283a405da5ae2cc7e944081ff033a2ba04b44cd5
Vikas Pogu <vikaspoguadf@gmail.com>
2020-01-23 16:47:45 -0500

    sixth commit

--- go.mod
+++ go.mod
@@ -3,10 +3,18 @@ module raspi-gocv
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/hybridgroup/mjpeg v0.0.0-20140228234708-4680f319790e
-	github.com/prometheus/client_golang v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/prometheus/client_golang v1.3.0
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.8.0 // indirect
 	gocv.io/x/gocv v0.22.0
-	golang.org/x/sys v0.0.0-20200117145432-59e60aa80a0c // indirect
+	golang.org/x/sys v0.0.0-20200122134326-e047566fdf82 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/hybridgroup/mjpeg"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 	"gocv.io/x/gocv"
+	"image"
 	"image/color"
-	"log"
-	"net/http"
+	"os"
 )
 
 var (
 	deviceID int
 	err      error
-	cam   *gocv.VideoCapture
+	cam      *gocv.VideoCapture
 	stream   *mjpeg.Stream
 )
 
+const MinimumArea = 3000
+
 func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	
 	// open cam
-	cam, err = gocv.VideoCaptureDevice(0)
+	cam, err = gocv.OpenVideoCapture(0)
 	if err != nil {
 		fmt.Printf("error opening video capture device: %v\n", deviceID)
 		return
@@ -32,44 +39,79 @@ func main() {
 	// start capturing
 	go capture()
 
-	fmt.Println("Capturing....")
+	log.Info("Capturing....")
 
-	// start http server
-	http.Handle("/", stream)
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		os.Getenv("user"): os.Getenv("password"),
+	}))
+
+	authorized.GET("/", func(c *gin.Context) {
+		stream.ServeHTTP(c.Writer, c.Request)
+	})
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	_ = r.Run()
 }
 
 func capture() {
+	window := gocv.NewWindow("Motion Window")
+	defer window.Close()
+
 	img := gocv.NewMat()
 	defer img.Close()
 
-	// load classifier to recognize faces
-	classifier := gocv.NewCascadeClassifier()
-	classifier.Load("./data/haarcascade_frontalface_alt2.xml")
+	imgDelta := gocv.NewMat()
+	defer imgDelta.Close()
 
-	defer classifier.Close()
+	imgThresh := gocv.NewMat()
+	defer imgThresh.Close()
 
+	mog2 := gocv.NewBackgroundSubtractorMOG2()
+	defer mog2.Close()
+
+	status := "Ready"
+
+	fmt.Printf("Start reading device: %v\n", deviceID)
 	for {
 		if ok := cam.Read(&img); !ok {
-			fmt.Printf("cannot read device %d\n", deviceID)
+			fmt.Printf("Device closed: %v\n", deviceID)
 			return
 		}
 		if img.Empty() {
 			continue
 		}
 
-		// color for the rect when faces detected
-		boxColor := color.RGBA{0, 255, 0, 0}
+		status = "Ready"
+		statusColor := color.RGBA{0, 255, 0, 0}
 
-		// detect faces
-		rects := classifier.DetectMultiScale(img)
+		// first phase of cleaning up image, obtain foreground only
+		mog2.Apply(img, &imgDelta)
 
-		// draw a rectangle around each face on the original image,
-		// along with text identifying as "Human"
-		for _, r := range rects {
-			gocv.Rectangle(&img, r, boxColor, 3)
+		// remaining cleanup of the image to use for finding contours.
+		// first use threshold
+		gocv.Threshold(imgDelta, &imgThresh, 25, 255, gocv.ThresholdBinary)
+
+		// then dilate
+		kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Pt(3, 3))
+		defer kernel.Close()
+		gocv.Dilate(imgThresh, &imgThresh, kernel)
+
+		// now find contours
+		contours := gocv.FindContours(imgThresh, gocv.RetrievalExternal, gocv.ChainApproxSimple)
+		for i, c := range contours {
+			area := gocv.ContourArea(c)
+			if area < MinimumArea {
+				continue
+			}
+
+			status = "Motion detected"
+			statusColor = color.RGBA{255, 0, 0, 0}
+			gocv.DrawContours(&img, contours, i, statusColor, 2)
+
+			rect := gocv.BoundingRect(c)
+			gocv.Rectangle(&img, rect, color.RGBA{0, 0, 255, 0}, 2)
 		}
+
+		gocv.PutText(&img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, statusColor, 2)
 
 		buf, _ := gocv.IMEncode(".jpg", img)
 		stream.UpdateJPEG(buf)
