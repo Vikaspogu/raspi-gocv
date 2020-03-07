6464b4ad2370f51c54af1812621228e9a7934587
Vikas Pogu <vikaspoguadf@gmail.com>
2020-03-07 16:08:36 -0500

    Adding new cascade model

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 	"image"
 	"image/color"
 	"os"
-	"path/filepath"
-	"raspi-gocv/vault"
 )
 
 var (
@@ -47,11 +45,13 @@ func main() {
 
 	log.Info("Capturing....")
 
-	username, password = vault.ReadSecret("secret/data/demo/userinfo")
+	//username, password = vault.ReadSecret("secret/data/demo/userinfo")
 
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		username: password,
+		//username: password,
+		"adminuser": "adminpass",
 	}))
+
 	authorized.GET("/", func(c *gin.Context) {
 		stream.ServeHTTP(c.Writer, c.Request)
 	})
@@ -60,39 +60,24 @@ func main() {
 }
 
 func capture() {
-	model := "data/res10_300x300_ssd_iter_140000.caffemodel"
-	config := "data/deploy.prototxt"
-	backend := gocv.NetBackendDefault
-	target := gocv.NetTargetCPU
+	xmlFile := "data/fullbody_recognition_model.xml"
+	// prepare image matrix
 	img := gocv.NewMat()
 	defer img.Close()
 
-	// open DNN object tracking model
-	net := gocv.ReadNet(model, config)
-	if net.Empty() {
-		log.Info("Error reading network model from : %v %v\n", model, config)
+	// color for the rect when faces detected
+	blue := color.RGBA{0, 0, 255, 0}
+
+	// load classifier to recognize faces
+	classifier := gocv.NewCascadeClassifier()
+	defer classifier.Close()
+
+	if !classifier.Load(xmlFile) {
+		log.Warn("Error reading cascade file: %v\n", xmlFile)
 		return
-	}
-	defer net.Close()
-	_ = net.SetPreferableBackend(backend)
-	_ = net.SetPreferableTarget(target)
-
-	var ratio float64
-	var mean gocv.Scalar
-	var swapRGB bool
-
-	if filepath.Ext(model) == ".caffemodel" {
-		ratio = 1.0
-		mean = gocv.NewScalar(104, 177, 123, 0)
-		swapRGB = false
-	} else {
-		ratio = 1.0 / 127.5
-		mean = gocv.NewScalar(127.5, 127.5, 127.5, 0)
-		swapRGB = true
 	}
 
 	log.Info("Start reading device: %v\n", deviceID)
-
 	for {
 		if ok := cam.Read(&img); !ok {
 			log.Info("Device closed: %v\n", deviceID)
@@ -102,39 +87,20 @@ func capture() {
 			continue
 		}
 
-		// convert image Mat to 300x300 blob that the object detector can analyze
-		blob := gocv.BlobFromImage(img, ratio, image.Pt(300, 300), mean, swapRGB, false)
+		// detect faces
+		rects := classifier.DetectMultiScale(img)
 
-		// feed the blob into the detector
-		net.SetInput(blob, "")
+		// draw a rectangle around each face on the original image,
+		// along with text identifying as "Human"
+		for _, r := range rects {
+			gocv.Rectangle(&img, r, blue, 3)
 
-		// run a forward pass thru the network
-		prob := net.Forward("")
-
-		performDetection(&img, prob)
-
-		prob.Close()
-		blob.Close()
+			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
+		}
 
 		buf, _ := gocv.IMEncode(".jpg", img)
 		stream.UpdateJPEG(buf)
-	}
-}
-
-// performDetection analyzes the results from the detector network,
-// which produces an output blob with a shape 1x1xNx7
-// where N is the number of detections, and each detection
-// is a vector of float values
-// [batchId, classId, confidence, left, top, right, bottom]
-func performDetection(frame *gocv.Mat, results gocv.Mat) {
-	for i := 0; i < results.Total(); i += 7 {
-		confidence := results.GetFloatAt(0, i+2)
-		if confidence > 0.5 {
-			left := int(results.GetFloatAt(0, i+3) * float32(frame.Cols()))
-			top := int(results.GetFloatAt(0, i+4) * float32(frame.Rows()))
-			right := int(results.GetFloatAt(0, i+5) * float32(frame.Cols()))
-			bottom := int(results.GetFloatAt(0, i+6) * float32(frame.Rows()))
-			gocv.Rectangle(frame, image.Rect(left, top, right, bottom), color.RGBA{0, 255, 0, 0}, 2)
-		}
 	}
 }
